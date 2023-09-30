8c9e752eedd077d749a3450d26dda611a8b4ecb5
Grant Edwards <grant.b.edwards@gmail.com>
2023-09-30 12:24:37 -0500

    Check body style attributes to try to determine if the email is being composed in plaintext or html mode. Only add custom CSS if it's html mode.

--- compose.js
+++ compose.js
@@ -1,16 +1,20 @@
-let marker_id = '#CustomCSS-add-on-marker';
-let style_elements = document.head.getElementsByTagName('style');
-let max = style_elements.length;
+let bs = document.body.style;
+let isHTML = !((bs.width==='72ch') && (bs.whiteSpace === 'pre-wrap') && (bs.fontFamily === 'monospace'));
 
-for (let i=max-1; i >=0 ; i--)
-    if (style_elements[i].textContent.startsWith(marker_id))
-        document.head.removeChild(style_elements[i]);
+if (isHTML) {
+    let marker_id = '#CustomCSS-add-on-marker';
+    let style_elements = document.head.getElementsByTagName('style');
+    let max = style_elements.length;
 
-function setCustomCSS(result) {
-    let style = document.createElement("style");
-    style.textContent = marker_id + '{}\n' + result.custom_css;
-    document.head.appendChild(style);
+    for (let i=max-1; i >=0 ; i--)
+        if (style_elements[i].textContent.startsWith(marker_id))
+            document.head.removeChild(style_elements[i]);
+
+    function setCustomCSS(result) {
+        let style = document.createElement("style");
+        style.textContent = marker_id + '{}\n' + result.custom_css;
+        document.head.appendChild(style);
+    }
+    let getting = browser.storage.local.get('custom_css');
+    getting.then(setCustomCSS)
 }
-let getting = browser.storage.local.get('custom_css');
-getting.then(setCustomCSS)
-
